d62016df266e33e96097edfe9d320cda7fd4c109
Shzylo <shzylo66@gmail.com>
2013-08-07 21:00:15 -0500

    Create WaterTile.java

---
+++ me/shzylo/mansionazazel/level/tile/WaterTile.java
@@ -0,0 +1,15 @@
+package me.shzylo.mansionazazelvisual.level.tile;
+
+import me.shzylo.mansionazazelvisual.gfx.Screen;
+import me.shzylo.mansionazazelvisual.gfx.Sprite;
+
+public class WaterTile extends Tile {
+
+  public WaterTile(Sprite sprite) {
+		super(sprite);
+	}
+
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x << 4, y << 4, this);
+	}
+}
