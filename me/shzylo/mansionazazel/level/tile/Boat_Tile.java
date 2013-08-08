5e7dda01616a6be4e4900f924f6acb59022568d1
Shzylo <shzylo66@gmail.com>
2013-08-07 21:10:12 -0500

    Create Boat_Tile.java

---
+++ me/shzylo/mansionazazel/level/tile/Boat_Tile.java
@@ -0,0 +1,19 @@
+package me.shzylo.mansionazazelvisual.level.tile;
+
+import me.shzylo.mansionazazelvisual.gfx.Screen;
+import me.shzylo.mansionazazelvisual.gfx.Sprite;
+
+public class Boat_Tile extends Tile {
+
+  public Boat_Tile(Sprite sprite) {
+		super(sprite);
+	}
+
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x << 4, y << 4, this);
+	}
+	
+	public boolean isSolid() {
+		return true;
+	}
+}
