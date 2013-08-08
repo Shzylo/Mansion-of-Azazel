f3171031219812a7bd7368ecf1df7da7314d0d10
Shzylo <shzylo66@gmail.com>
2013-08-07 21:05:42 -0500

    Create TreeTop_BottomLeftTile.java

---
+++ me/shzylo/mansionazazel/level/tile/TreeTop_BottomLeftTile.java
@@ -0,0 +1,19 @@
+package me.shzylo.mansionazazelvisual.level.tile;
+
+import me.shzylo.mansionazazelvisual.gfx.Screen;
+import me.shzylo.mansionazazelvisual.gfx.Sprite;
+
+public class TreeTop_BottomLeftTile extends Tile {
+
+  public TreeTop_BottomLeftTile(Sprite sprite) {
+		super(sprite);
+	}
+
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x << 4, y << 4, this);
+	}
+	
+	public boolean isSolid() {
+		return true;
+	}
+}
