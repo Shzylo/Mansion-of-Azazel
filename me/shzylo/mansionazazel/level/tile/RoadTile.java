3c39d3b2e96254c9862221edfcd3df8c95faa9b8
Shzylo <shzylo66@gmail.com>
2013-09-17 16:55:04 -0700

    Update RoadTile.java

--- me/shzylo/mansionazazel/level/tile/RoadTile.java
+++ me/shzylo/mansionazazel/level/tile/RoadTile.java
@@ -1,14 +1,16 @@
-package me.shzylo.mansionazazelvisual.level.tile;
+package me.shzylo.mansionazazel.level.tile;
 
-import me.shzylo.mansionazazelvisual.gfx.Screen;
-import me.shzylo.mansionazazelvisual.gfx.Sprite;
+import me.shzylo.mansionazazel.graphics.Screen;
+import me.shzylo.mansionazazel.graphics.Sprite;
 
+/**
+ * Just the plain road.
+ */
 public class RoadTile extends Tile {
-
-  public RoadTile(Sprite sprite) {
+	public RoadTile(Sprite sprite) {
 		super(sprite);
 	}
-
+	
 	public void render(int x, int y, Screen screen) {
 		screen.renderTile(x << 4, y << 4, this);
 	}
