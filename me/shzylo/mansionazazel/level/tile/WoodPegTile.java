6123cedafd6026a2e1a3933f65387a41b37e26f1
Shzylo <shzylo66@gmail.com>
2013-09-17 16:45:33 -0700

    Update WoodPegTile.java

--- me/shzylo/mansionazazel/level/tile/WoodPegTile.java
+++ me/shzylo/mansionazazel/level/tile/WoodPegTile.java
@@ -1,15 +1,21 @@
-package me.shzylo.mansionazazelvisual.level.tile;
+package me.shzylo.mansionazazel.level.tile;
 
-import me.shzylo.mansionazazelvisual.gfx.Screen;
-import me.shzylo.mansionazazelvisual.gfx.Sprite;
+import me.shzylo.mansionazazel.graphics.Screen;
+import me.shzylo.mansionazazel.graphics.Sprite;
 
+/**
+ * Wood pegs, for when the wood tile goes over water
+ */
 public class WoodPegTile extends Tile {
-
-  public WoodPegTile(Sprite sprite) {
+	public WoodPegTile(Sprite sprite) {
 		super(sprite);
 	}
 
 	public void render(int x, int y, Screen screen) {
 		screen.renderTile(x << 4, y << 4, this);
+	}
+	
+	public boolean isSolid() {
+		return true;
 	}
 }
