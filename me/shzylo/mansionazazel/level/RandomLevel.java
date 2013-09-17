848d4e1fb29691b02b5ef0d1be63acfd0dc122d0
Shzylo <shzylo66@gmail.com>
2013-09-17 16:43:36 -0700

    Update RandomLevel.java

--- me/shzylo/mansionazazel/level/RandomLevel.java
+++ me/shzylo/mansionazazel/level/RandomLevel.java
@@ -1,15 +1,18 @@
-package me.shzylo.mansionazazelvisual.level;
+package me.shzylo.mansionazazel.level;
 
 import java.util.Random;
 
+/**
+ * A randomly generated level
+ */
 public class RandomLevel extends Level {
+	public static final Random random = new Random();
 
-  private static final Random random = new Random();
-	
 	public RandomLevel(int width, int height) {
 		super(width, height);
 	}
-
+	
+	@Override
 	protected void generateLevel() {
 		for(int y = 0; y < height; y++) {
 			for(int x = 0; x < width; x++) {
