d0bd4b4d50a3469732c408eefbf230537f11a6c6
Shzylo <shzylo66@gmail.com>
2013-09-17 16:53:54 -0700

    Update GrassTile.java

--- me/shzylo/mansionazazel/level/tile/GrassTile.java
+++ me/shzylo/mansionazazel/level/tile/GrassTile.java
@@ -1,14 +1,16 @@
-package me.shzylo.mansionazazelvisual.level.tile;
+package me.shzylo.mansionazazel.level.tile;
 
-import me.shzylo.mansionazazelvisual.gfx.Screen;
-import me.shzylo.mansionazazelvisual.gfx.Sprite;
+import me.shzylo.mansionazazel.graphics.Screen;
+import me.shzylo.mansionazazel.graphics.Sprite;
 
+/**
+ * A grass tile.
+ */
 public class GrassTile extends Tile {
-  
 	public GrassTile(Sprite sprite) {
 		super(sprite);
 	}
-	
+
 	public void render(int x, int y, Screen screen) {
 		screen.renderTile(x << 4, y << 4, this);
 	}
