04f63bea6bcb861e94c1098e4eececbccf048988
Shzylo <shzylo66@gmail.com>
2013-09-17 16:55:36 -0700

    Update Tile.java

--- me/shzylo/mansionazazel/level/tile/Tile.java
+++ me/shzylo/mansionazazel/level/tile/Tile.java
@@ -1,56 +1,56 @@
-package me.shzylo.mansionazazelvisual.level.tile;
+package me.shzylo.mansionazazel.level.tile;
 
-import me.shzylo.mansionazazelvisual.gfx.Screen;
-import me.shzylo.mansionazazelvisual.gfx.Sprite;
+import me.shzylo.mansionazazel.graphics.Screen;
+import me.shzylo.mansionazazel.graphics.Sprite;
 
+/**
+ * Class that contains all tiles
+ */
 public class Tile {
-
-  public int x, y;
+	public int x, y;
 	public Sprite sprite;
 
-	public static Tile voidTile = new VoidTile(Sprite.voidSprite);
+	public static Tile nothing = new VoidTile(Sprite.nothing);
 	public static Tile grass = new GrassTile(Sprite.grass);
 	public static Tile bush = new BushTile(Sprite.bush);
-	
+
 	public static Tile treeTrunk = new TreeTrunkTile(Sprite.treeTrunk);
-	public static Tile treeTop = new TreeTopTile(Sprite.treeTop);
-	public static Tile treeTop_L = new TreeTop_LeftTile(Sprite.treeTop_L);
-	public static Tile treeTop_T = new TreeTop_TopTile(Sprite.treeTop_T);
-	public static Tile treeTop_R = new TreeTop_RightTile(Sprite.treeTop_R);
-	public static Tile treeTop_B = new TreeTop_BottomTile(Sprite.treeTop_B);
-	public static Tile treeTop_TR = new TreeTop_TopRightTile(Sprite.treeTop_TR);
-	public static Tile treeTop_TL = new TreeTop_TopLeftTile(Sprite.treeTop_TL);
-	public static Tile treeTop_BR = new TreeTop_BottomRightTile(Sprite.treeTop_BR);
-	public static Tile treeTop_BL = new TreeTop_BottomLeftTile(Sprite.treeTop_BL);
-	
+	public static Tile treeTop = new TreeLeafTile(Sprite.treeLeaf);
+	public static Tile treeTop_L = new TreeLeaf_LeftTile(Sprite.treeLeaf_L);
+	public static Tile treeTop_T = new TreeLeaf_TopTile(Sprite.treeLeaf_T);
+	public static Tile treeTop_R = new TreeLeaf_RightTile(Sprite.treeLeaf_R);
+	public static Tile treeTop_B = new TreeLeaf_BottomTile(Sprite.treeLeaf_B);
+	public static Tile treeTop_TR = new TreeLeaf_TopRightTile(Sprite.treeLeaf_TR);
+	public static Tile treeTop_TL = new TreeLeaf_TopLeftTile(Sprite.treeLeaf_TL);
+	public static Tile treeTop_BR = new TreeLeaf_BottomRightTile(Sprite.treeLeaf_BR);
+	public static Tile treeTop_BL = new TreeLeaf_BottomLeftTile(Sprite.treeLeaf_BL);
+
 	public static Tile road = new RoadTile(Sprite.road);
 	public static Tile roadDash = new RoadDashTile(Sprite.roadDash);
-	
+
 	public static Tile water = new WaterTile(Sprite.water);
-	
+
 	public static Tile dirtRoadDicipateL = new DirtRoadDicipateLTile(Sprite.dirtRoadDicipateL);
 	public static Tile dirtRoad = new DirtRoadTile(Sprite.dirtRoad);
 	public static Tile dirtRoadDicipateR = new DirtRoadDicipateRTile(Sprite.dirtRoadDicipateR);
-	
+
 	public static Tile cobble = new CobbleTile(Sprite.cobble);
 	public static Tile woodPeg = new WoodPegTile(Sprite.woodPeg);
 	public static Tile wood = new WoodTile(Sprite.wood);
 	public static Tile woodL = new WoodLeftTile(Sprite.woodL);
-	
+
 	public static Tile boat = new BoatTile(Sprite.boat);
 	public static Tile boat_ = new Boat_Tile(Sprite.boat_);
-	
+
 	public static Tile car_0 = new Car_0Tile(Sprite.car_0);
 	public static Tile car_1 = new Car_1Tile(Sprite.car_1);
 	public static Tile car_2 = new Car_2Tile(Sprite.car_2);
 	public static Tile car_3 = new Car_3Tile(Sprite.car_3);
 	public static Tile car_4 = new Car_4Tile(Sprite.car_4);
 	public static Tile car_5 = new Car_5Tile(Sprite.car_5);
-
-	
 	
 	public static final int grassColor = 0xFFB6FF00;
-	
+
 	public static final int treeTrunkColor = 0xFF7F3300;
 	public static final int treeTopColor = 0xFF007F0E;
 	public static final int treeTop_LColor = 0xFF00CC06;
@@ -61,16 +61,16 @@ public class Tile {
 	public static final int treeTop_TRColor = 0xFF00D80A;
 	public static final int treeTop_BLColor = 0xFF00E507;
 	public static final int treeTop_BRColor = 0xFF00F204;
-	
+
 	public static final int roadColor = 0xFF303030;
 	public static final int roadDashColor = 0xFF3A3A3A;
-	
+
 	public static final int waterColor = 0xFF0026FF;
-	
+
 	public static final int dirtRoadColor = 0xFF8C3800;
 	public static final int dirtRoadDicipateLColor = 0xFF993A00;
 	public static final int dirtRoadDicipateRColor = 0xFFA53C00;
-	
+
 	public static final int cobbleColor = 0xFF9E0000;
 	public static final int woodPegColor = 0xFFC14D00;
  	public static final int woodColor = 0xFF913A00;
@@ -78,22 +78,33 @@ public class Tile {
  	
  	public static final int boatColor = 0xFFD85600;
  	public static final int boat_Color = 0xFFE55700;
-	
+
 	public static final int car_0Color = 0xFFFF0000;
 	public static final int car_1Color = 0xFFF20000;
 	public static final int car_2Color = 0xFFE50000;
 	public static final int car_3Color = 0xFFD80000;
 	public static final int car_4Color = 0xFFCC0000;
 	public static final int car_5Color = 0xFFBF0000;
-
+	
 	public Tile(Sprite sprite) {
 		this.sprite = sprite;
 	}
-
+	
+	/**
+	 * Render the tile
+	 * @param x
+	 * @param y
+	 * @param screen
+	 */
 	public void render(int x, int y, Screen screen) {
-
 	}
 	
+	/**
+	 * Checks if the tile is solid<br>
+	 * If solid the player cannot walk through it<br>
+	 * If not specified, it returns false
+	 * @return
+	 */
 	public boolean isSolid() {
 		return false;
 	}
