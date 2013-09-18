c42af950df1fa3d376e7177b79536765235d1fcb
Shzylo <shzylo66@gmail.com>
2013-09-17 17:09:18 -0700

    Update and rename me/shzylo/mansionazazel/gfx/SpriteSheet.java to me/shzylo/mansionazazel/graphics/SpriteSheet.java

--- me/shzylo/mansionazazel/graphics/SpriteSheet.java
+++ me/shzylo/mansionazazel/graphics/SpriteSheet.java
@@ -1,18 +1,21 @@
-package me.shzylo.mansionazazelvisual.gfx;
+package me.shzylo.mansionazazel.graphics;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * Sheets for sprites to be made from
+ */
 public class SpriteSheet {
-
-  private String path;
+	private String path;
 	public final int SIZE;
 	public int[] pixels;
 
 	public static SpriteSheet tiles = new SpriteSheet("/sheets/tiles.png", 256);
 	public static SpriteSheet player = new SpriteSheet("/sheets/player.png", 96);
+	public static SpriteSheet ghost = new SpriteSheet("/sheets/ghost.png", 96);
 
 	public SpriteSheet(String path, int size) {
 		this.path = path;
@@ -21,6 +24,9 @@ public class SpriteSheet {
 		load();
 	}
 
+	/**
+	 * Load the specified SpriteSheet
+	 */
 	private void load() {
 		try {
 			BufferedImage image = ImageIO.read(SpriteSheet.class.getResource(path));
