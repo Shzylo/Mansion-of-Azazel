dc6a68584fbc9d8c0bdcf80e1372d746a9e250f0
Shzylo <shzylo66@gmail.com>
2013-09-17 17:00:38 -0700

    Update and rename TreeTop_TopLeftTile.java to TreeLeaf_TopLeftTile.java

---
+++ me/shzylo/mansionazazel/level/tile/TreeLeaf_TopLeftTile.java
@@ -0,0 +1,22 @@
+package me.shzylo.mansionazazel.level.tile;
+
+import me.shzylo.mansionazazel.graphics.Screen;
+import me.shzylo.mansionazazel.graphics.Sprite;
+
+/**
+ * The top-left leafs of a tree<br><br>
+ * <b>Solid Tile</b>
+ */
+public class TreeLeaf_TopLeftTile extends Tile {
+	public TreeLeaf_TopLeftTile(Sprite sprite) {
+		super(sprite);
+	}
+
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x << 4, y << 4, this);
+	}
+	
+	public boolean isSolid() {
+		return true;
+	}
+}
