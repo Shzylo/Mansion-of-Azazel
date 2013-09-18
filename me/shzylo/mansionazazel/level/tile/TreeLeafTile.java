4cfaac6f3c879cdeeb0fcb4249177f704064c497
Shzylo <shzylo66@gmail.com>
2013-09-17 17:02:19 -0700

    Update and rename TreeTop_TopTile.java to TreeLeafTile.java

---
+++ me/shzylo/mansionazazel/level/tile/TreeLeafTile.java
@@ -0,0 +1,22 @@
+package me.shzylo.mansionazazel.level.tile;
+
+import me.shzylo.mansionazazel.graphics.Screen;
+import me.shzylo.mansionazazel.graphics.Sprite;
+
+/**
+ * Base leaf of a Tree<br><br>
+ * <b>Solid Tile</b>
+ */
+public class TreeLeafTile extends Tile {
+	public TreeLeafTile(Sprite sprite) {
+		super(sprite);
+	}
+
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x << 4, y << 4, this);
+	}
+	
+	public boolean isSolid() {
+		return true;
+	}
+}
