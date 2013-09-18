ec26618ed6235b1c5d4debb1ac1ff838810f6d2f
Shzylo <shzylo66@gmail.com>
2013-09-17 17:00:08 -0700

    Update and rename TreeTop_RightTile.java to TreeLeaf_RightTile.java

---
+++ me/shzylo/mansionazazel/level/tile/TreeLeaf_RightTile.java
@@ -0,0 +1,22 @@
+package me.shzylo.mansionazazel.level.tile;
+
+import me.shzylo.mansionazazel.graphics.Screen;
+import me.shzylo.mansionazazel.graphics.Sprite;
+
+/**
+ * The right-side leafs of a tree<br><br>
+ * <b>Solid Tile</b>
+ */
+public class TreeLeaf_RightTile extends Tile {
+	public TreeLeaf_RightTile(Sprite sprite) {
+		super(sprite);
+	}
+
+	public void render(int x, int y, Screen screen) {
+		screen.renderTile(x << 4, y << 4, this);
+	}
+	
+	public boolean isSolid() {
+		return true;
+	}
+}
