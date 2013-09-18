453e93f900dda8fb09122e3eec4fbbc44e8d0446
Shzylo <shzylo66@gmail.com>
2013-09-17 17:06:32 -0700

    Update Keyboard.java

--- me/shzylo/mansionazazel/input/Keyboard.java
+++ me/shzylo/mansionazazel/input/Keyboard.java
@@ -1,29 +1,33 @@
-package me.shzylo.mansionazazelvisual.input;
+package me.shzylo.mansionazazel.input;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+/**
+ * The user's input for the character.
+ */
 public class Keyboard implements KeyListener {
-
-  private boolean[] keys = new boolean[120];
+	public boolean[] keys = new boolean[120];
 	public boolean up, down, left, right, debug;
 
+	/**
+	 * Update the keys
+	 */
 	public void update() {
 		up = keys[KeyEvent.VK_UP] || keys[KeyEvent.VK_W];
-		down = keys[KeyEvent.VK_DOWN] || keys[KeyEvent.VK_S];
 		left = keys[KeyEvent.VK_LEFT] || keys[KeyEvent.VK_A];
+		down = keys[KeyEvent.VK_DOWN] || keys[KeyEvent.VK_S];
 		right = keys[KeyEvent.VK_RIGHT] || keys[KeyEvent.VK_D];
-
+		
 		debug = keys[KeyEvent.VK_F1];
 	}
 
 	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == 112) { // 112 = F1
-			if (!debug) {
+		if (e.getKeyCode() == 112) {
+			if (!debug)
 				keys[e.getKeyCode()] = true;
-			} else {
+			else
 				keys[e.getKeyCode()] = false;
-			}
 		} else {
 			if (e.getKeyCode() < keys.length) {
 				keys[e.getKeyCode()] = true;
@@ -40,4 +44,5 @@ public class Keyboard implements KeyListener {
 	public void keyTyped(KeyEvent e) {
 		keys[e.getKeyCode()] = true;
 	}
+
 }
