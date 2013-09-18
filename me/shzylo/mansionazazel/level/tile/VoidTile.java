5182b6d639748a66c44f02f104dd657296a90a35
Shzylo <shzylo66@gmail.com>
2013-09-17 17:04:15 -0700

    Update VoidTile.java

--- me/shzylo/mansionazazel/level/tile/VoidTile.java
+++ me/shzylo/mansionazazel/level/tile/VoidTile.java
@@ -1,16 +1,22 @@
-package me.shzylo.mansionazazelvisual.level.tile;
+package me.shzylo.mansionazazel.level.tile;
 
-import me.shzylo.mansionazazelvisual.gfx.Screen;
-import me.shzylo.mansionazazelvisual.gfx.Sprite;
-import me.shzylo.mansionazazelvisual.level.tile.Tile;
+import me.shzylo.mansionazazel.graphics.Screen;
+import me.shzylo.mansionazazel.graphics.Sprite;
 
+/**
+ * A Tile of absolutely nothing, just in case the player gets out of the map
+ */
 public class VoidTile extends Tile {
-  
+
 	public VoidTile(Sprite sprite) {
 		super(sprite);
 	}
-	
+
 	public void render(int x, int y, Screen screen) {
 		screen.renderTile(x << 4, y << 4, this);
+	}
+	
+	public boolean isSolid() {
+		return true;
 	}
 }
